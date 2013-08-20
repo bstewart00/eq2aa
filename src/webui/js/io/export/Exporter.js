7136832a076a6a09ee3e4fe5887a88f43eee6569
beetny <eq2aa@beetny.com>
2013-08-20 22:32:32 +1000

    New command

--- src/webui/js/io/export/Exporter.js
+++ src/webui/js/io/export/Exporter.js
@@ -7,3 +7,29 @@ Beetny.EQ2AA.Exporter = Class.extend({
 			return this._version + ";" + classModel.createHash()
 		}
 	});
+
+Beetny.EQ2AA.UrlHashExporter = {}; 
+Beetny.EQ2AA.XmlExporter = Class.extend({
+      init : function (version) {
+         this._version = version || Beetny.EQ2AA.GameVersions.LatestUpdate
+      },
+      exportClass : function (classModel) {
+         /*
+          * Add "Exported by Beetny comment, including URL link to build"
+          * <aa game="eq2">
+          * For each tree in [valid tree order]
+          *    <alternateadvancements typenum="{TYPENUM}">
+          *    set order = 1
+          *    For each spent AA, sorted by
+          *          - number of prerequisites and/or x, y location ascending
+          *          <alternateadvancement order="{i}" treeID="{TreeSoeID}" id="{AASoeId}"/>
+          *          order++
+          * </alternateadvancements>
+          * </aa>
+          * 
+          * Send file as download somehow. Make POST with XML string to server, to send back as binary data?
+          */
+         
+         return this._version + ";" + classModel.createHash()
+      }
+   });
--- src/webui/js/ui/Commands.js
+++ src/webui/js/ui/Commands.js
@@ -3,22 +3,22 @@ Beetny.EQ2AA.AATreeViewerCommand = Class.extend({
 		init : function (id, name, description) {
 			this.id = id;
 			this.name = name;
-			this.description = description
+			this.description = description;
 		},
 		render : function () {
 			var template = '<li class="command {commandId}" title="{Title}"><a href="javascript:;">{Name}</a></li>';
 			var html = template.replace("{commandId}", this.id).replace("{Title}", this.description).replace("{Name}", this.name);
-			return $(html)
+			return $(html);
 		},
 		execute : function (aaTreeViewer) {}
 
 	});
 Beetny.EQ2AA.ResetAllCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
 		init : function () {
-			this._super("resetAll", "Reset All", "Resets all trees in the current class.")
+			this._super("resetAll", "Reset All", "Resets all trees in the current class.");
 		},
 		execute : function (aaTreeViewer) {
-			aaTreeViewer.resetClass()
+			aaTreeViewer.resetClass();
 		}
 	});
 Beetny.EQ2AA.ResetTreeCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
@@ -26,7 +26,7 @@ Beetny.EQ2AA.ResetTreeCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
 			this._super("resetTree", "Reset Tree", "Resets the currently selected tree.")
 		},
 		execute : function (aaTreeViewer) {
-			aaTreeViewer.resetActiveTree()
+			aaTreeViewer.resetActiveTree();
 		}
 	});
 Beetny.EQ2AA.ExportCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
@@ -36,9 +36,23 @@ Beetny.EQ2AA.ExportCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
 		render : function () {
 			var element = this._super();
 			$("a", element).attr("target", "_blank");
-			return element
+			return element;
 		}
 	});
+	
+Beetny.EQ2AA.ExportXmlCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
+      init : function () {
+         this._super("export-xml", "Export to File", "Creates .aa file that can be imported directly into EQ2.")
+      },
+      execute : function (aaTreeViewer) {
+         console.log("GO");
+      },
+      render : function () {
+         var element = this._super();
+         $("a", element).attr("target", "_blank");
+         return element;
+      }
+   });
 Beetny.EQ2AA.PrintCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
 		init : function () {
 			this._super("print", "Print", "Creates a text summary suitable for printing.")
@@ -46,10 +60,10 @@ Beetny.EQ2AA.PrintCommand = Beetny.EQ2AA.AATreeViewerCommand.extend({
 		render : function () {
 			var element = this._super();
 			$("a", element).attr("target", "_blank");
-			return element
+			return element;
 		},
 		execute : function (aaTreeViewer) {
-			aaTreeViewer.printClass()
+			aaTreeViewer.printClass();
 		}
 	});
-Beetny.EQ2AA.AATreeViewerCommands = [new Beetny.EQ2AA.ResetAllCommand, new Beetny.EQ2AA.ResetTreeCommand, new Beetny.EQ2AA.ExportCommand, new Beetny.EQ2AA.PrintCommand];
+Beetny.EQ2AA.AATreeViewerCommands = [new Beetny.EQ2AA.ResetAllCommand, new Beetny.EQ2AA.ResetTreeCommand, new Beetny.EQ2AA.ExportCommand,  new Beetny.EQ2AA.ExportXmlCommand, new Beetny.EQ2AA.PrintCommand];
