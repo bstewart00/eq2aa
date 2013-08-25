f6be09270f49233db1bcf3e9b4814f4b2d4015fb
beetny <eq2aa@beetny.com>
2013-08-25 20:49:33 +1000

    XmlBuilder refactor

--- src/webui/js/io/export/Exporter.js
+++ src/webui/js/io/export/Exporter.js
@@ -10,99 +10,30 @@ Beetny.EQ2AA.Exporter = Class.extend({
 
 Beetny.EQ2AA.XmlExporter = Class.extend({
    init : function() {
-   },
-
-   _formatXml : function(xml) {
-      var formatted = '';
-      var reg = /(>)(<)(\/*)/g;
-      xml = xml.replace(reg, '$1\r\n$2$3');
-      var pad = 0;
-      jQuery.each(xml.split('\r\n'), function(index, node) {
-         var indent = 0;
-         if (node.match(/.+<\/\w[^>]*>$/)) {
-            indent = 0;
-         } else if (node.match(/^<\/\w/)) {
-            if (pad != 0) {
-               pad -= 1;
-            }
-         } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
-            indent = 1;
-         } else {
-            indent = 0;
-         }
-
-         var padding = '';
-         for (var i = 0; i < pad; i++) {
-            padding += '  ';
-         }
-
-         formatted += padding + node + '\r\n';
-         pad += indent;
-      });
-
-      return formatted;
+      this._xmlBuilder = new Beetny.XmlBuilder();
    },
 
    exportClass : function(classModel) {
-      // creates a Document object with root "<report>"
-      var doc = document.implementation.createDocument("", "", null);
-      this._appendExportComment(doc);
-
-      var aaElem = doc.createElementNS("", "aa");
-
-      var a = document.createAttribute("game");
-      a.nodeValue = "eq2";
-      aaElem.setAttributeNode(a);
-      doc.appendChild(aaElem);
-
+      this._xmlBuilder.appendComment('Exported by http://www.beetny.com/eq2aa');
+      var aaElem = this._xmlBuilder.appendChild('aa', { 'game': 'eq2' });
+      
       classModel.trees.filter(function(tree) {
          return tree.getTotalPointsSpent() > 0;
       }).forEach(function(tree) {
          this._exportTree(tree, aaElem);
       }, this);
-
-      var declaration = '<?xml version="1.0" encoding="UTF-8"?>';
-      var xml = declaration + new XMLSerializer().serializeToString(doc);
-      return this._formatXml(xml);
-   },
-
-   _appendExportComment : function(parentElement) {
-      var comment = 'Exported by http://www.beetny.com/eq2aa'
-      var a = document.createComment();
-      a.nodeValue = comment;
-      parentElement.appendChild(a);
+      
+      return this._xmlBuilder.prettify().build();
    },
 
    _exportTree : function(tree, parentElement) {
-      var element = document.createElementNS("", "alternateadvancements");
-      var a = document.createAttribute("typenum");
-      a.nodeValue = tree.typeNum();
-      element.setAttributeNode(a);
-
-      parentElement.appendChild(element);
+      var treeElem = this._xmlBuilder.appendChild('alternateadvancements', { 'typenum': tree.typeNum() }, parentElement)
       
       var order = 1;
       tree.aa.filter(function (aa) {
          return aa.level > 0;
       }).forEach(function (aa) {
-         this._exportAA(aa, order++, tree.soe_id, element);
+         this._xmlBuilder.appendChild('alternateadvancement', { 'order': order++, 'treeID': tree.soe_id, 'id':aa.soe_id }, treeElem);
       }, this);
-   },
-   
-   _exportAA: function (aa, order, treeId, parentElement) {
-      var element = document.createElementNS("", "alternateadvancement");
-      var orderAttr = document.createAttribute("order");
-      orderAttr.nodeValue = order;
-      element.setAttributeNode(orderAttr);
-      
-      var treeIdAttr = document.createAttribute("treeID");
-      treeIdAttr.nodeValue = treeId;
-      element.setAttributeNode(treeIdAttr);
-         
-      var aaIdAttr = document.createAttribute("id");
-      aaIdAttr.nodeValue = aa.soe_id;
-      element.setAttributeNode(aaIdAttr);
-      
-      parentElement.appendChild(element);
    }
 });
---
+++ src/webui/js/util/XmlBuilder.js
@@ -0,0 +1,73 @@
+Beetny.XmlBuilder = Class.extend({
+   init : function() {
+      this._doc = document.implementation.createDocument("", "", null);
+   },
+
+   appendComment : function(text, parent) {
+      parent = parent || this._doc;
+
+      var node = this._doc.createComment();
+      node.nodeValue = text;
+      parent.appendChild(node);
+   },
+
+   appendChild : function(tagName, attributeMap, parent) {
+      parent = parent || this._doc;
+      attributeMap = attributeMap || {};
+
+      var element = this._doc.createElementNS("", tagName);
+
+      Object.items(attributeMap).forEach(function(kvp) {
+         var attr = document.createAttribute(kvp.key);
+         attr.nodeValue = kvp.value;
+         element.setAttributeNode(attr);
+      });
+
+      parent.appendChild(element);
+
+      return element;
+   },
+
+   prettify : function() {
+      this._prettyPrint = true;
+      return this;
+   },
+
+   build : function() {
+      var declaration = '<?xml version="1.0" encoding="UTF-8"?>';
+      var xml = declaration + new XMLSerializer().serializeToString(this._doc);
+
+      return this._prettyPrint ? this._formatXml(xml) : xml;
+   },
+
+   _formatXml : function(xml) {
+      var formatted = '';
+      var reg = /(>)(<)(\/*)/g;
+      xml = xml.replace(reg, '$1\r\n$2$3');
+      var pad = 0;
+      jQuery.each(xml.split('\r\n'), function(index, node) {
+         var indent = 0;
+         if (node.match(/.+<\/\w[^>]*>$/)) {
+            indent = 0;
+         } else if (node.match(/^<\/\w/)) {
+            if (pad != 0) {
+               pad -= 1;
+            }
+         } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+            indent = 1;
+         } else {
+            indent = 0;
+         }
+
+         var padding = '';
+         for (var i = 0; i < pad; i++) {
+            padding += '  ';
+         }
+
+         formatted += padding + node + '\r\n';
+         pad += indent;
+      });
+
+      return formatted;
+   }
+}); 
