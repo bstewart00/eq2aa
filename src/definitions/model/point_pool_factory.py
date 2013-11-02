c6a9a17f3b9926b3521d8941aba11d585661db20
beetny <eq2aa@beetny.com>
2013-11-02 12:59:25 +1000

    Tests

--- src/definitions/model/point_pool_factory.py
+++ src/definitions/model/point_pool_factory.py
@@ -1,14 +1,36 @@
 class PointPoolFactory:
     def __init__(self):
         self._pool_order = { "AA": 0, "Prestige": 1, "Tradeskill": 2, "TradeskillPrestige": 3, "Warder": 4 }
+        self._global_aa_max = 320
     
     def create(self, trees):
-        points = []
+        points = self._get_point_pools(trees)
         
         unordered = list(set([self._map_tree_to_pool_name(t) for t in trees]))
         ordered_point_pools = sorted(unordered, key=lambda t: self._pool_order[t])
         
         return points, ordered_point_pools
+    
+    def _make_pool(self, name, max_points):
+        return { "name": name, "max": max_points }
+    
+    def _get_point_pools(self, trees):
+        pools = {}
+        
+        for t in trees:
+            if t.type in ["Prestige", "Tradeskill", "TradeskillPrestige"]:
+                pools[t.type] = self._make_pool(t.type, t.max_points)
+            elif t.type == "Warder":
+                pools[t.type] = { "name": t.type, "child_pools": self._get_child_pools(trees) }
+            else:
+                pools["AA"] = self._make_pool("AA", self._global_aa_max)
+                
+        return pools
+    
+    def _filter_warder_trees(self, trees):
+        for t in trees:
+            if t.is_warder_tree:
+                yield t
     
     def _map_tree_to_pool_name(self, tree):
         valid_tree_types = ["Warder", "Prestige", "TradeskillPrestige", "Tradeskill"]
@@ -17,3 +39,10 @@ class PointPoolFactory:
             return tree.type
         else:
             return "AA"
+        
+    def _get_child_pools(self, trees):
+        pools = {}
+        for warder_tree in self._filter_warder_trees(trees):
+            pools[warder_tree.name] = self._make_pool(warder_tree.name, warder_tree.max_points)
+        return pools
+        
--- src/definitions/tests/point_pool_factory_tests.py
+++ src/definitions/tests/point_pool_factory_tests.py
@@ -6,15 +6,55 @@ class TestPointPoolFactory(unittest.TestCase):
     def setUp(self):
         self.sut = PointPoolFactory()
         
-    def test_One(self):
-        trees = self._create_trees_of_type(["Archetype", "Tradeskill", "Prestige", "TradeskillPrestige", "Warder"])
+    def test_ordered_point_pools(self):
+        trees = list(self._create_trees_of_type(["Archetype", "Tradeskill", "Prestige", "TradeskillPrestige", "Warder"]))
         
         points, ordered_point_pools = self.sut.create(trees)
         
         self.assertEqual(ordered_point_pools, ["AA", "Prestige", "Tradeskill", "TradeskillPrestige", "Warder"])
         
+    def test_points_has_name_and_max_points(self):
+        trees = list(self._create_trees([("Archetype", 20), ("Prestige", 30), ("Tradeskill", 40), ("TradeskillPrestige", 50)]))
+        
+        points, ordered_point_pools = self.sut.create(trees)
+        
+        self.assertEqual(points["AA"], { "name": "AA", "max": 320 })
+        self.assertEqual(points["Prestige"], { "name": "Prestige", "max": 30 })
+        self.assertEqual(points["Tradeskill"], { "name": "Tradeskill", "max": 40 })
+        self.assertEqual(points["TradeskillPrestige"], { "name": "TradeskillPrestige", "max": 50 })
+        
+    def test_points_warder_has_child_pools(self):
+        trees = list(self._create_warder_trees([("Mystical", 20), ("Avian", 30), ("Aquatic", 40)]))
+        
+        points, ordered_point_pools = self.sut.create(trees)
+        
+        expected_child_pools = {'Aquatic': {'max': 40, 'name': 'Aquatic'},
+                                'Avian': {'max': 30, 'name': 'Avian'},
+                                'Mystical': {'max': 20, 'name': 'Mystical'}}
+        
+        self.assertEqual(points["Warder"], { "name": "Warder", "child_pools": expected_child_pools })
+        
+        
     def _create_trees_of_type(self, types):
         for t in types:
             obj = MagicMock()
             obj.type = t
+            yield obj
+            
+    def _create_trees(self, tuples):
+        for t in tuples:
+            obj = MagicMock()
+            obj.name = t[0]
+            obj.type = t[0]
+            obj.max_points = t[1]
+            
+            yield obj
+            
+    def _create_warder_trees(self, tuples):
+        for t in tuples:
+            obj = MagicMock()
+            obj.name = t[0]
+            obj.type = "Warder"
+            obj.max_points = t[1]
+            
             yield obj
